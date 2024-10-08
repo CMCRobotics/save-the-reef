d67621da0088d83541cb1771a04375c9b3471a70
Brice Copy <500789+bcopy@users.noreply.github.com>
2024-10-08 10:35:29 +0200

    Add presentation structure

---
+++ src/reveal-presentation.js
@@ -0,0 +1,28 @@
+import Reveal from 'reveal.js';
+import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import 'reveal.js/dist/reveal.css';
+import 'reveal.js/dist/theme/white.css';
+
+
+Reveal.initialize({
+    plugins: [ Markdown ],
+    progress: false,
+    controls: true
+  }).then(() => {
+    $(document).ready(
+        function(){
+            $('body').on('submit','form',function() {
+                var sessionCode = $('#session_code')[0].value;
+                $("section[data-microsquad]").each(
+                    function(){
+                        var iframeUrl = new URL( $(this).data("background-iframe") );
+                        iframeUrl.searchParams.set("sc", sessionCode);
+                        $(this).data("background-iframe",iframeUrl.href);
+                        console.log("Now "+$(this).data("background-iframe"));
+                    }
+                );
+                return false;
+            });
+        } 
+    );
+});
